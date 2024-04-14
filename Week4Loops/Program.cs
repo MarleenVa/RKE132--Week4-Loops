12ec04b803358038719fd2bfb6f430d82bc3f3e3
Marleen Vaiksaar. <marleen.vaiksaar@tktk.ee>
2024-04-14 21:01:36 +0300

    first commit

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//FOR-LOOP
+//DRY-PRINCIPLE - DO not repeat yourself
+
+int i = 0;
+for(i = 0; i < 5; i++) //i ++ = i +i
+{
+    Console.WriteLine("We don't need no education");
+}
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopactive = true;
+
+while(loopactive)
+{
+    cpuRandom = rnd.Next(1, 4);
+        Console.WriteLine($"cpu has generated {cpuRandom}");
+        Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int usernumber = Int32.Parse(Console.ReadLine());
+
+    if (usernumber == cpuRandom )
+    {
+        Console.WriteLine("Congratiulations, you won!");
+        loopactive = false;
+        break;
+
+    } else
+    {
+        Console.WriteLine("Oops. Try again.");
+    }
+}
+Console.WriteLine("Have a nice day");
